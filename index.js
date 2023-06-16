a8e5e95f0b24534ad10d8a460bb4ded09a11443b
Collins <collinskiarie13@outlook.com>
2023-06-16 12:52:26 +0300

    Completed task

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (numb1, numb2) => {
+    return (2000/100);
+}
+
+const square = (x) => {
+     x = 2
+    return (x ** 2);
+}
+
+const add = (a,b) => {
+    a = 3;
+    b = 4;
+
+    return (a+b)
+}
